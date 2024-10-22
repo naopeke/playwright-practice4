3477f543cd5a76018a6d89625611a6801d60ccb1
naopeke <naopeke@protonmail.com>
2024-10-22 12:24:37 +0200

    api post

--- tests/apiTesting.spec.ts
+++ tests/apiTesting.spec.ts
@@ -1,8 +1,27 @@
 import { test, expect } from '@playwright/test';
+import exp from 'constants';
 
 test('API GET Request', async ({ request })=>{
     const response = await request.get('https://reqres.in/api/users/2');
+    
     expect(response.status()).toBe(200);
+    
     const text = await response.text();
     expect(text).toContain('Janet');
+    
+    console.log(await response.json());
+})
+
+test('API POST Request', async ({ request })=>{
+    const response = await request.post('https://reqres.in/api/users',{
+        data:
+            {
+                "name": "Lati",
+                "job": "manager"
+            }
+    });
+    expect(response.status()).toBe(201);
+    const text = await response.text();
+    expect(text).toContain('Lati');
+    console.log(await response.json());
 })
