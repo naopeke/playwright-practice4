92cac646c1a905c84b4ea20751248aa0bb92aba3
naopeke <naopeke@protonmail.com>
2024-10-24 15:21:52 +0200

    api

--- tests/apiTesting2.spec.ts
+++ tests/apiTesting2.spec.ts
@@ -7,6 +7,7 @@ test.describe('Automation Exercise', ()=>{
     test('API 1: Get All Products List', async ({ request })=>{
         const response = await request.get('https://automationexercise.com/api/productsList');
         expect (response.status()).toBe(200);
+        
         const json = await response.json();
         console.log(json);
     });
@@ -14,6 +15,7 @@ test.describe('Automation Exercise', ()=>{
     test('API 2: POST To All Products List', async({ request})=>{
         const response = await request.post('https://automationexercise.com/api/productsList');
         expect (response.status()).toBe(405);
+        
         const json = await response.json();
         expect(json).toHaveProperty('This request method is not supported.');
     })
