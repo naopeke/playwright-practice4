37c1e8d4cc3ae7c84cdf7c62329e1cc2ddf91715
naopeke <naopeke@protonmail.com>
2024-10-25 10:19:29 +0200

    exercise

--- tests/apiTesting2.spec.ts
+++ tests/apiTesting2.spec.ts
@@ -6,7 +6,7 @@ test.describe('Automation Exercise', ()=>{
 
     test('API 1: Get All Products List', async ({ request })=>{
         const response = await request.get('https://automationexercise.com/api/productsList');
-        expect (response.status()).toBe(200);
+        expect(response.status()).toBe(200);
         
         const json = await response.json();
         console.log(json);
@@ -22,7 +22,7 @@ test.describe('Automation Exercise', ()=>{
 
     test('API 3: Get All Brands List', async ({ request })=>{
         const response = await request.get('https://automationexercise.com/api/brandsList');
-        expect (response.status()).toBe(200);
+        expect(response.status()).toBe(200);
 
         const json = await response.json();
         console.log(json);
@@ -30,13 +30,44 @@ test.describe('Automation Exercise', ()=>{
 
     test('API 4: PUT To All Brands List', async ({ request })=>{
         const response = await  request.put('https://automationexercise.com/api/brandsList');
-        expect (response.status()).toBe(405);
+        expect(response.status()).toBe(405);
 
         const json = await response.json();
-        expect (json).toHaveProperty('This request method is not supported.');
+        expect(json).toHaveProperty('This request method is not supported.');
     })
 
     test('API 5: POST To Search Product', async ({ request })=>{
-        const response = await request.post(' https://automationexercise.com/api/searchProduct');
+        const response = await request.post('https://automationexercise.com/api/searchProduct', {
+            data: {
+                search_product : 'tshirt'
+            }
+        });
+
+        expect(response.status()).toBe(200);
+        const json = await response.json();
+        console.log(json);
+    })
+
+    test('API 6: POST To Search Product without search_product parameter', async ({ request })=>{
+        const response = await request.post('https://automationexercise.com/api/searchProduct', {
+            data : {}
+        });
+
+        expect(response.status()).toBe(400);
+        const json = await response.json();
+        expect(json).toHaveProperty('Bad request, search_product parameter is missing in POST request.');
+    })
+
+    test('API 7: POST To Verify Login with valid details', async ({ request })=>{
+        const response = await request.post('https://automationexercise.com/api/verifyLogin', {
+            data : {
+                email : 'test@example.com',
+                password : '123456'
+            }
+        });
+
+        expect(response.status()).toBe(200);
+        const json = await response.json();
+        expect(json).toHaveProperty('User exists!');
     })
 })
