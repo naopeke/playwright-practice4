d430d6e66925ed3c904140ffd04c64829836d4e1
naopeke <naopeke@protonmail.com>
2024-11-15 18:50:52 +0100

    pom

--- pom/productPage.ts
+++ pom/productPage.ts
@@ -7,58 +7,75 @@ class ProductPage {
         this.request = request;
     }
 
+    //1
     async getAllProducts(){
         const response = await this.request.get('https://automationexercise.com/api/productsList');
         return response;
     }
 
+    //2
     async postToProductsList(data: Record<string, any>){
         const response = await this.request.post('https://automationexercise.com/api/productsList', { data });
         return response;
     }
 
+    //3
     async getAllBrands(){
         const response = await this.request.get('https://automationexercise.com/api/brandsList');
         return response;
     }
 
+    //4
     async putAllBrands(){
         const response = await this.request.post('https://automationexercise.com/api/brandsList');
         return response;
     }
 
+    //5
     async postSearchProducts(data: Record<string, any>){
         const response = await this.request.post('https://automationexercise.com/api/searchProduct', { data });
         return response;
     }
 
+    //6
     async postSearchProductsWithoutParam(){
         const response = await this.request.post('https://automationexercise.com/api/searchProduct');
         return response;
     }
 
+    //7
     async postToVerifyLogin(data: Record<string, any>){
         const response = await this.request.post('https://automationexercise.com/api/verifyLogin', { data });
         return response;
     }
 
+    //8
     async postToVerifyLoginWitoutParam(){
         const response = await this.request.post('https://automationexercise.com/api/verifyLogin');
         return response;
     }
     
+    //9
     async deleteVeryfyLogin(){
         const response = await this.request.delete('https://automationexercise.com/api/verifyLogin');
         return response;
     }
 
+    //10
     async postToVerifyLoginWithInvalidData(data:Record<string,any>){
         const response = await this.request.post('https://automationexercise.com/api/verifyLogin', { data });
         return response;
     }
 
+    //11
     async postUserAccount(data:Record<string,any>){
         const response = await this.request.post('https://automationexercise.com/api/createAccount', { data });
+        return response;
+    }
+
+    //12
+    async deleteUserAccount(data:Record<string, any>){
+        const response = await this.request.delete('https://automationexercise.com/api/deleteAccount', { data });
         return response;
     }
 
